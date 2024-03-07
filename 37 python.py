02cc9819640e72dfcc9547d60fd6f24d0c35488a
Robinkoko <161907448+Robinkoko@users.noreply.github.com>
2024-03-07 21:09:58 +0900

    3/7 python

---
+++ 37 python.py
@@ -0,0 +1,13 @@
+##for i in range(10):
+##    print(f"{i+1}번째 문자")
+##for i in range(10000):
+##    print(i)
+##    if i == 555:
+##            break
+s = input("비밀번호를 입력해 주세요")
+if s == 123456:
+    print("확인되었습니다")
+
+else:
+        print(f"{s}는 비밀번호가 아닙니다")
+   
