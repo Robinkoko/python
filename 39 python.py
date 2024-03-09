43e9ae6adb64e3c94169194c6918b66d9d99ef78
Robinkoko <161907448+Robinkoko@users.noreply.github.com>
2024-03-09 11:51:43 +0900

    3/9 python

---
+++ 39 python.py
@@ -0,0 +1,88 @@
+##for i in range(10000):
+##i=1
+##while i < 83:
+##    i = i+1
+##    print("*" * i)
+
+##import turtle
+##t = turtle.Turtle()
+##t.shape("turtle")
+##for i in range(1000):
+##    t.fd(int(i))
+##    t.right(90)
+
+##a = 123
+##b = "hello world"
+##c = True
+##print(a,type(a))
+##print(b,type(b))
+##print(c,type(c))
+
+##print("원주율은%f이다"%3.141592653589793238462643382)
+
+##name = "홍길동"
+##bir = 5
+##print(f"내이름은 {name}이고 {bir}월에 태어났다")
+##print("내이름은 {0}이고 {1}월에 태어났다".format(name,5))
+
+##a =10
+##b =2
+##print("%d %d %d" %( a+b, a-b, a*b))
+
+##name = input("이름 : ")
+##age = int(input("나이 : "))
+##tall = float(input("키 : "))
+##print("==친구 소개하기==\n제 친구의 이름은 %s이고, 나이는 %d살이에요.\n키는 %.1fcm예요."%(name,age,tall))
+##print(f"==자기 소개하기==\n이름 : {name}\n나이 : {age}\n키 : {tall}")
+
+##play = input("경기 종목: ")
+##grade = input("학년: ")
+##winner = input("이긴 반: ")
+##loser = input("진 반: ")
+##print("")
+##print("============================================================")
+##print(f"오늘 시큐브 초등학교에서 {play} 경기가 열렸습니다.\n{grade}학년 {winner}반과 {loser}반이 피튀기는(?) 경기를 펼쳤고\n{winner}반이 승리했습니다.")
+##print("============================================================")
+
+##L = int(input("L판매 개수 : "))
+##M = int(input("M판매 개수 : "))
+##S = int(input("S판매 개수 : "))
+##sum = 2500 * L
+##sum = sum + 1000 * M
+##sum = sum + 500 * S
+##print(f"오늘의 총 매출은 {sum}원 입니다")
+
+##a = int(input("ㄴ"))
+##b = int(input("ㄴ"))
+##c = int(input("ㄴ"))
+##print("평균 : %.2f" %float(((a+b+c)/3)))
+
+##n = int(input("ㄴ"))
+##for i in range(10):
+##    print(n*(i+1), end = ' ')
+
+##print("%10s %10d %10.2f" %('cat',3,3.14))
+##print("%10s %10d %10.3f" %('dog',127,1.234))
+##print("%10s %10d %10.1f" %('turtle',543212,10.1))
+
+##h = int(input("height : "))
+##w = int(input("width : "))
+##print (float(h * w /2))
+
+##i = int(input("inch : "))
+##print(float(2.54 * i ))
+
+##a = int(input("a : "))
+##b = int(input("b : "))
+##print(int(a ** b))
+
+##a=int(input())
+##b=int(input())
+##print("%d - %d = %d" %(a,b,a-b))
+##print("%d * %d = %d" %(a,b,a*b))
+##print("%d / %d = %.2f" %(a,b,a/b))
+##print("%d // %d = %d" %(a,b,a//b))
+##print("%d %% %d = %d" %(a,b,a%b))
+
+
+      
