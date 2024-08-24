9ed569f0cef0eced886867566fdd9d631ff32929
Robinkoko <161907448+Robinkoko@users.noreply.github.com>
2024-08-24 11:45:11 +0900

    08/24python

---
+++ main.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from court import *
+from ball import *
+win = Tk()
+width, height = 745, 374
+win.title("tennis game")
+win.geometry("745x374+150+150")
+win.resizable(False,False)
+court = Court(win,745,374,"court.png")
+x1,y1 = width/2,height/2
+x2,y2 = x1+30,y1+30
+ball = Ball(court,x1,y1,x2,y2)
+def play_game() :
+    ball.move_ball()
+    win.after(50, play_game)
+play_game()
+win.mainloop()
